c9732db552b28f4094897d193713587bdf18b7ed
buegare <brunobuegare@gmail.com>
2016-11-07 20:25:36 -0200

    Create association between tweets and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107220048) do
+ActiveRecord::Schema.define(version: 20161107222456) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
