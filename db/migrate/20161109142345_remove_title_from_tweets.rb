4b547cf3dce6b9daaa8c27d916d2c69bdac514da
buegare <brunobuegare@gmail.com>
2016-11-09 12:26:13 -0200

    Remove column title from Tweet

---
+++ db/migrate/20161109142345_remove_title_from_tweets.rb
@@ -0,0 +1,5 @@
+class RemoveTitleFromTweets < ActiveRecord::Migration[5.0]
+  def change
+  	remove_column :tweets, :title, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107222456) do
+ActiveRecord::Schema.define(version: 20161109142345) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
