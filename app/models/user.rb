f439464bc8b44470a94f8780379e36e61ff2a25d
buegare <brunobuegare@gmail.com>
2016-11-24 19:14:06 -0200

    Undo the relation between User and Friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :tweets
-  has_many :friendships
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
