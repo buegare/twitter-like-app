5e56ad4a36a4016150004f81923a124466e89f8a
buegare <brunobuegare@gmail.com>
2016-11-24 18:25:13 -0200

    Create model Friendship and relation with User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :tweets
+  has_many :friendships
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112134746) do
+ActiveRecord::Schema.define(version: 20161124202157) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "following"
+    t.integer  "followers"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text     "body"
