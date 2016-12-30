9d409df62f585faa452051f3efd2539f87bf9c9a
buegare <brunobuegare@gmail.com>
2016-12-29 16:29:13 -0800

    Implement the right frienship status on suggention box

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,31 @@
+module UsersHelper
+
+	def check_friendship_status(user)
+		puts(is_signed_user_followed_by_user?(user), signed_user_follows_user?(user))
+
+		if is_signed_user_followed_by_user?(user) == true and signed_user_follows_user?(user) == true
+			return "You follow each other"
+		elsif is_signed_user_followed_by_user?(user) == true and signed_user_follows_user?(user) == false
+			return "Follows you"
+		elsif is_signed_user_followed_by_user?(user) == false and signed_user_follows_user?(user) == true
+			return "Following"
+		else
+			return ""
+		end
+	end
+
+	def is_signed_user_followed_by_user?(user)
+		user.follows.each do |user|
+			return true if user.following == current_user.id
+		end
+		return false
+	end
+
+	def signed_user_follows_user?(user)
+		user.followers.each do |user|
+			return true if user.is_followed_by == current_user.id
+		end
+		return false
+	end
+
+end
