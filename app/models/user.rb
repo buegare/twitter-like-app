23b1dcdf6143a04acff892248edf776c0b72ef99
Bruno Buegare <brunobuegare@gmail.com>
2017-02-03 18:22:27 -0800

    Fix where images are saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
 
   has_attached_file :image, styles: { large: "1600x900>", medium: "300x300>", thumb: "50x50#" },
                 default_url: "image.png",
-  							url: "/assets/users/:id/:style/:basename.:extension",
-  							path: ":rails_root/public/assets/users/:id/:style/:basename.:extension"
+  							url: "/assets/users/:id/image/:style/:basename.:extension",
+  							path: ":rails_root/public/assets/users/:id/image/:style/:basename.:extension"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   has_attached_file :bigger_image, styles: { large: "1600x900>"},
-                url: "/assets/users/:id/:style/:basename.:extension",
-                path: ":rails_root/public/assets/users/:id/:style/:basename.:extension"
+                url: "/assets/users/:id/bigger_image/:style/:basename.:extension",
+                path: ":rails_root/public/assets/users/:id/bigger_image/:style/:basename.:extension"
   validates_attachment_content_type :bigger_image, content_type: /\Aimage\/.*\z/
 
   has_many :tweets
